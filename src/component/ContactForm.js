e31a453284036faf858afe46975ad38071525a03
Jaeeun Kim <dmjfhe12@gmail.com>
2024-09-02 20:35:10 +0100

    Making a contact

---
+++ src/component/ContactForm.js
@@ -0,0 +1,45 @@
+import React, { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import { useDispatch } from 'react-redux';
+
+const ContactForm = () => {
+  const [name, setName] = useState('');
+  const [phoneNumber, setPhoneNumber] = useState(0);
+  const dispatch = useDispatch();
+  const addContact = (event) => {
+    event.preventDefault(); //버튼 클릭시 리프래쉬 막기
+    dispatch({
+      type: 'ADD_CONTACT',
+      payload: { name, phoneNumber },
+    });
+  };
+  return (
+    <div>
+      <Form onSubmit={addContact}>
+        <Form.Group className="mb-3" controlId="formName">
+          <Form.Label>이름</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="이름을 입력해주세요"
+            onChange={(event) => setName(event.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formContact">
+          <Form.Label>전화번호</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="전화번호를 입력해주세요"
+            onChange={(event) => setPhoneNumber(event.target.value)}
+          />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          추가
+        </Button>
+      </Form>
+    </div>
+  );
+};
+
+export default ContactForm;
